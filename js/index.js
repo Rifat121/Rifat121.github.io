345c2472068eb98d0d3aed7fdfbeae88e68f7a29
Rifat121 <jahedulalamrifat@gmail.com>
2023-10-09 15:28:17 +0600

    ..

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ const contentElements = document.querySelectorAll('.content-element');
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('animate'); // Add a CSS class for animation
+      entry.target.classList.add('animate'); 
     //   observer.unobserve(entry.target); // Stop observing once animated
     }
     else
